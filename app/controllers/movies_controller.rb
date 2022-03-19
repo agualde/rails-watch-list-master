9e4082de67b909f0c194732218708038c0eb4a39
Andres Gualde <andres@gualde.com>
2022-03-19 18:59:17 +0100

    testing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,16 +3,16 @@ class MoviesController < ApplicationController
     
     @list = List.find(1)
     @bookmark = Bookmark.new
-    @movies = Movie.all.limit(100)
+    @movies = Movie.all.limit(50)
 
         if params[:query].present?
-          @movies = @movies.global_search(params[:query]).limit(25)
+          @movies = @movies.global_search(params[:query]).limit(15)
           respond_to do |format|
             format.html
             format.text { render partial: 'movies/list', locals: { movies: @movies, bookmark: @bookmark }, formats: [:html] }
           end
         else
-          @movies = Movie.all.limit(100)
+          @movies = Movie.all.limit(50)
         end
   end
 
